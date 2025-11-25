16605b1ca09d2ce9bcae336015add1cc814786e7
21Kl3v1s21 <klevis25sfj@gmail.com>
2025-11-25 19:37:27 +0100

    hero section, easy for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import gsap from "gsap";
 import NavBar from "./components/NavBar";
+import HeroSection from "./sections/HeroSection";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <HeroSection />
+      <div className="h-dvh border border-black"></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
